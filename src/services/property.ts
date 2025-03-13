60b5fa51f9ce93e95cea8bb4ab5b6069b081117f
mik284 <mikeykamau222@gmail.com>
2025-03-13 10:48:51 +0300

    fix: Correct data extraction from fetchAllProperties response

--- src/services/property.ts
+++ src/services/property.ts
@@ -1,67 +1,79 @@
 import { ParamsType } from '@ant-design/pro-components';
-import axios from 'axios';
 import axiosInstance from './request';
 
 export const createNewProperty = async (leadProperty) => {
-    try {
-        const response = await axiosInstance.post(`${BASE_URL}/properties`, leadProperty,
-            {
-                headers: {
-                    Authorization: `Bearer ${JSON.parse(localStorage.getItem('property_token') || '{}')
-                        }`,
-                },
-            }
-        );
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.post(
+      `${BASE_URL}/properties`,
+      leadProperty,
+      {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(
+            localStorage.getItem('property_token') || '{}',
+          )}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 export const fetchAllProperties = async (token?: ParamsType) => {
-    try {
-        const response = await axiosInstance.get(`${BASE_URL}/properties`, {
-            headers: {
-                Authorization: `Bearer ${token || JSON.parse(localStorage.getItem('property_token') || '{}')
-                    }`,
-            },
-        });
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get(`${BASE_URL}/properties`, {
+      headers: {
+        Authorization: `Bearer ${
+          token || JSON.parse(localStorage.getItem('property_token') || '{}')
+        }`,
+      },
+    });
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export const updateProperty = async (
-    propertyId: string,
-    propertyData: any,
-    token?: ParamsType,
+  propertyId: string,
+  propertyData: any,
+  token?: ParamsType,
 ) => {
-    try {
-        const response = await axiosInstance.put(`${BASE_URL}/properties/${propertyId}`, propertyData, {
-            headers: {
-                Authorization: `Bearer ${token || JSON.parse(localStorage.getItem('property_token') || '{}')
-                    }`,
-            },
-        });
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.put(
+      `${BASE_URL}/properties/${propertyId}`,
+      propertyData,
+      {
+        headers: {
+          Authorization: `Bearer ${
+            token || JSON.parse(localStorage.getItem('property_token') || '{}')
+          }`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 export const deleteProperty = async (userId: string, token?: ParamsType) => {
-    try {
-        const response = await axiosInstance.delete(`${BASE_URL}/properties/${userId}`, {
-            headers: {
-                Authorization: `Bearer ${token || JSON.parse(localStorage.getItem('property_token') || '{}')
-                    }`,
-            },
-        });
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.delete(
+      `${BASE_URL}/properties/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${
+            token || JSON.parse(localStorage.getItem('property_token') || '{}')
+          }`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
