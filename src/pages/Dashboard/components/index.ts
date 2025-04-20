3e2ee16decdf3ec4531beced7ae98b8d2d5c8813
mik284 <mikeykamau222@gmail.com>
2025-04-20 09:00:00 +0300

    feat: add index file to export dashboard components

---
+++ src/pages/Dashboard/components/index.ts
@@ -0,0 +1,5 @@
+export { default as LeadStatusChart } from './LeadStatusChart';
+export { default as PropertyTypeChart } from './PropertyTypeChart';
+export { default as RecentActivities } from './RecentActivities';
+export { default as SalesTrendChart } from './SalesTrendChart';
+export { default as StatsCards } from './StatsCards';
